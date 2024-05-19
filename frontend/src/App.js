440ef737817f3a8b76312958070c31c9ad12aaf5
sanjeevbhu22 <sanjeevkumar.bhumca22@gmail.com>
2024-05-19 12:52:08 +0530

    first commit to the git hub

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+        <h1> Hi ! THIS is only for the testing purpose of the best uploads</h1>
+        <ul>
+          <li>Home</li>
+          <li>Contact us</li>
+          <li>About us</li>
+        </ul>
+    </div>
+  );
+}
+
+export default App;
